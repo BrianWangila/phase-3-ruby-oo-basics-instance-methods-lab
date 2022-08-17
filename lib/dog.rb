a1c9b1b3a20a6ade331f0a5e5e82e531c4e6efd0
Brian Wangila <bwanjalah@gmail.com>
2022-08-17 20:08:30 +0300

    commit tree 6a75e20de254ab4546bfdb49067f0c0a14496121

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+# Dot Notation
+
+class Dog
+  #Class body
+
+  #Instance method definition
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
+
+snoopy = Dog.new
+snoopy.bark
